b2eb3fde8d08dc5b3d524d860f93bd627f04b98d
tochepi <christystoch@gmail.com>
2021-08-15 13:40:13 -0500

    change js base

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName,playerAttack,playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
