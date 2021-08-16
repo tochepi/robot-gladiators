dd50f3ecf879de0054c25e715f66d699b1c61473
tochepi <christystoch@gmail.com>
2021-08-16 14:45:11 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,16 +3,14 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName,playerAttack,playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // Alert players that they are starting the round
 window.alert("Welcome to Robot Gladiators!");
 
-var fight = function() {
+var fight = function(enemyName) {
     // If player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
         // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
@@ -20,14 +18,14 @@ var fight = function() {
 
         // Log a resulting message to the console so we know that it worked.
         console.log (
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
         );
 
         // Check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
         } else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
         }
 
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
@@ -35,7 +33,7 @@ var fight = function() {
 
         // Log a resulting message to the console so we know that it worked.
         console.log (
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
 
         // Check player's health
@@ -66,6 +64,20 @@ var fight = function() {
         }
 };
 
+for (var i = 0; i <enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log (i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+// console.log(playerName,playerAttack,playerHealth);
+
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-fight();
+//Game States
+// "WIN" - Player robot has defeated all enemy robots
+// * Fight all enemy robots
+
+for(var i = 0; i <enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
